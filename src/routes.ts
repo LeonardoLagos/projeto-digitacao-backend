0697e1e3e82d04b5f23d51af40bac93656fdb068
Leonardo Lagos <leonardolagos1928@gmail.com>
2023-12-14 11:08:01 -0400

    Implementação CI/CD: quinto teste

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,7 @@ routes.get("/historico/textos", new HistoricoUsuariosController().buscaHistorico
 routes.post("/historico/teclas", new HistoricoTeclasDigitadasController().cadastraHistoricoTeclasDigitadas);
 routes.get("/historico/teclas", new HistoricoTeclasDigitadasController().retornaPorcentagemTeclasDigitadasPorUsuario);
 routes.get("/erros/quantidade", new HistoricoUsuariosController().buscaQuantidadeErrosPorUsuario);
+////
 //
 
 export default routes;
